8c137bbe75ff195969d3e176e4314789d9564ee6
ali50m <ali50m@live.com>
2018-10-30 17:10:03 +0800

    Update py3SimpleHTTPServerWithUpload.py
    设置了新的监听端口port

--- py3SimpleHTTPServerWithUpload.py
+++ py3SimpleHTTPServerWithUpload.py
@@ -319,7 +319,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
 def test(HandlerClass = SimpleHTTPRequestHandler,
 		 ServerClass = http.server.HTTPServer):
-	http.server.test(HandlerClass, ServerClass)
+	http.server.test(HandlerClass, ServerClass, port=5888)
 
 if __name__ == '__main__':
 	test()
